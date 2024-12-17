4c0999172c828d0f8ca87752cbbae3ebd4cb9b36
Matheus Barbato <matheus.leoonel@gmail.com>
2024-12-17 13:49:36 -0300

    first commit

---
+++ src/main/java/com/mlb/Lonas/LonasApplication.java
@@ -0,0 +1,13 @@
+package com.mlb.Lonas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LonasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LonasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlb/Lonas/LonasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlb.Lonas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LonasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
