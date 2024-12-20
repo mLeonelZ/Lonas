48bc53cdfd98f0b5a0c9d33a51b6bf95b6aa94f5
Matheus Barbato <matheus.leoonel@gmail.com>
2024-12-20 17:56:19 -0300

    feat: cria os endpoints para CRUD e injeta a dependencia do SERVICE e REPOSITORY

--- src/main/java/com/mlb/Lonas/Vinilona/Controller/VinilonaController.java
+++ src/main/java/com/mlb/Lonas/Vinilona/Controller/VinilonaController.java
@@ -1,16 +1,57 @@
 package com.mlb.Lonas.Vinilona.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mlb.Lonas.Vinilona.Model.VinilonaModel;
+import com.mlb.Lonas.Vinilona.Service.VinilonaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/vinilona")
 public class VinilonaController {
+
+    private VinilonaService vinilonaService;
+
+    public VinilonaController(VinilonaService vinilonaService) {
+        this.vinilonaService = vinilonaService;
+    }
 
     @GetMapping("/teste")
     public String teste(){
         return "Teste";
+    }
+
+    // Adicionar Lona
+    @PostMapping("/criar")
+    public String criar(){
+        return "lona criada";
+    }
+
+
+    // Listar todas as lonas da maior para a menor
+    @GetMapping("/listar")
+    public List<VinilonaModel> listar(){
+        return vinilonaService.listar();
+    }
+
+
+    // Listar lonas por comprimento x largura
+    @GetMapping("/listarComp")
+    public String listarCompLarg(){
+        return "lona criada";
+    }
+
+
+    // Alterar lona por id
+    @PutMapping("/alterarID")
+    public String alterar(){
+        return "lona alterada";
+    }
+
+    // deletar lona
+    @DeleteMapping("/deletarID")
+    public String deletar(){
+        return "lona deletada";
     }
 
 
--- src/main/java/com/mlb/Lonas/Vinilona/Service/VinilonaService.java
+++ src/main/java/com/mlb/Lonas/Vinilona/Service/VinilonaService.java
@@ -1,4 +1,27 @@
 package com.mlb.Lonas.Vinilona.Service;
 
+import com.mlb.Lonas.Vinilona.Model.VinilonaModel;
+import com.mlb.Lonas.Vinilona.Repository.VinilonaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class VinilonaService {
+
+    // Faz a ligação do Banco de Dados com o Service
+    // Permite que o Service tenha acesso ao Banco de dados
+    // inicializando uma nova instância
+    private VinilonaRepository vinilonaRepository;
+
+    public VinilonaService(VinilonaRepository vinilonaRepository) {
+        this.vinilonaRepository = vinilonaRepository;
+    }
+
+    // Listar todas as lonas
+    public List<VinilonaModel> listar(){
+         return vinilonaRepository.findAll();
+    }
+
+
 }
