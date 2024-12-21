f03cca70b79369ae5f0ddbc402097e8a7642c277
Matheus Barbato <matheus.leoonel@gmail.com>
2024-12-20 21:13:54 -0300

    feat: cria o metodo findByComprimentoAndLargura

--- src/main/java/com/mlb/Lonas/Vinilona/Repository/VinilonaRepository.java
+++ src/main/java/com/mlb/Lonas/Vinilona/Repository/VinilonaRepository.java
@@ -2,6 +2,14 @@ package com.mlb.Lonas.Vinilona.Repository;
 
 import com.mlb.Lonas.Vinilona.Model.VinilonaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface VinilonaRepository extends JpaRepository<VinilonaModel,Long> {
+    List<VinilonaModel> findByComprimentoAndLargura(double comprimento,double largura);
+    // Cria um metodo para buscar o comprimento e a largura
+    // Os nomes devem ser os MESMOS que estao na CLASSE MODEL
+    // E como eu quero fazer isso utilizando 2 paramentros
+    // É preciso usar And depois do primeiro parametro
+
+
 }
